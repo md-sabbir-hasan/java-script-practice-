923dcd33c773b4f7fab1318c25c65d7e542b785d
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2025-12-31 12:19:03 +0600

    done

--- final-project-bootstrap/materials/js/extarnal.js
+++ final-project-bootstrap/materials/js/extarnal.js
@@ -15,3 +15,25 @@ function showContent(imgSrc, title, desc) {
   // Add fade-in class
   panel.classList.add('fade-in');
 }
+
+
+// degree-page
+
+
+function showPage(index) {
+  const pages = document.querySelectorAll('.degree-page');
+  const buttons = document.querySelectorAll('.page-btn');
+
+  pages.forEach((page, i) => {
+    page.classList.toggle('active', i === index);
+  });
+
+  buttons.forEach((btn, i) => {
+    btn.classList.toggle('active', i === index);
+  });
+}
+
+
+
+
+
